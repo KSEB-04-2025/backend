d68caf76ede7373ec48daa171d679028f91a8d4c
Lee Wonbin <ah0416@Naver.com>
2025-08-05 14:43:05 +0900

    refactor: api https 설정

--- src/main/java/com/backend/core/config/swagger/SwaggerConfig.java
+++ src/main/java/com/backend/core/config/swagger/SwaggerConfig.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
+                .addServersItem(new Server().url("https://zezeone-sf.site"))
                 .info(new Info()
                         .title("프로젝트 API 명세서")
                         .description("AI 기반 품질 분류 시스템 API 문서입니다.")
