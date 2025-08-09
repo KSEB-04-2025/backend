f04e11c2a0df0aa930a02385b87e3fcdceb5cac5
Lee Wonbin <ah0416@Naver.com>
2025-08-09 23:14:03 +0900

    fix: CORS 설정 및 세션 체크 로직 수정

--- src/main/java/com/backend/core/interceptor/SessionCheckInterceptor.java
+++ src/main/java/com/backend/core/interceptor/SessionCheckInterceptor.java
@@ -9,6 +9,10 @@ public class SessionCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+
         HttpSession session = request.getSession(false); // 기존 세션만 조회
         Boolean isAdmin = (session != null) ? (Boolean) session.getAttribute("isAdmin") : null;
 
